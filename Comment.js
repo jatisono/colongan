2791a0c4bfe3e6d2b4ce5ef6be3e83a2a5360c4d
jatisono <jatisono@live.com>
2019-03-05 21:12:41 +0800

    Add files via upload

---
+++ Comment.js
@@ -0,0 +1,158 @@
+﻿var normal="abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXZY1234567890"
+var balik="ɐqɔpǝⅎƃɥᴉɾʞʅɯuodbɹsʇnʌʍxzʎ∀ꓭϽᗡƎᖵ⅁HIᒋꓘ⅂ꟽNOԀꝹꓤSꓕՈɅϺXZ⅄⇂↊↋ߤ59𝘓860"
+var bulat="ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓩⓨⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓏⓎ➊➋➌➍➎➏➐➑➒➓"
+var bulat2="ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓩⓨⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓏⓎ➀➁➂➃➄➅➆➇➈➉"
+var bulat3="ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓩⓨⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓏⓎ⓵⓶⓷⓸⓹⓺⓻⓼⓽⓾"
+var smallcap="ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxᴢʏABCDEFGHIJKLMNOPQRSTUVWXZY1234567890"
+var cool="αß¢ḓℯḟℊ♄ḯʝкłღη☺℘ⓠґ﹩тυ√ẘϰℨ⑂Ѧℬ☾Ð€ℱḠℋℐJḲḺℳℵϴℙℚℜ$✝ṲṼШẌẐ¥123456子890"
+var symbol="ᗩᖲᑕᕍᗴℱᘐᖺᓮᒎḰᒪᗰﬡᗝᖰᕋᖇᔕ♈ᘎᐯᙡჯᔓ૪ᗩᗷᑕᗪᕮℱᘐᕼᓰᒎḰᒪᗰᘉ〇ᖘႳᖇᔕ☂ᘮⅤᙡ᙭ᔓᎩ1234567890"
+var rusia="aЬcdєfgнїjкlмиоpqґšтцvшxzџABCDЄFБHЇJКLMИОPQЯ₴TЦVШЖZУ12З4567890"
+var rusia4="𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕫𝕪𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏ℤ𝕐𝟙𝟚𝟛𝟜𝟝𝟞𝟟𝟠𝟡𝟘-?><@#$%^&*()_+:\/"
+var latin="𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔃𝔂𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓩𝓨1234567890"
+var grafiti="𝖆𝖇𝖈𝖉𝖊𝖋𝖌𝖍𝖎𝖏𝖐𝖑𝖒𝖓𝖔𝖕𝖖𝖗𝖘𝖙𝖚𝖛𝖜𝖝𝖟𝖞𝕬𝕭𝕮𝕯𝕰𝕱𝕲𝕳𝕴𝕵𝕶𝕷𝕸𝕹𝕺𝕻𝕼𝕽𝕾𝕿𝖀𝖁𝖂𝖃𝖅𝖄1234567890"
+var latin2="𝒶𝒷𝒸𝒹𝑒𝒻𝑔𝒽𝒾𝒿𝓀𝓁𝓂𝓃𝑜𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓏𝓎𝒜𝐵𝒞𝒟𝐸𝐹𝒢𝐻𝐼𝒥𝒦𝐿𝑀𝒩𝒪𝒫𝒬𝑅𝒮𝒯𝒰𝒱𝒲𝒳𝒵𝒴𝟣𝟤𝟥𝟦𝟧𝟨𝟩𝟪𝟫𝟢"
+var bold="ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｚｙＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＺＹ１２３４５６７８９０"
+var kotak="🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅉🅈🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅉🅈1234567890"
+var reverse="098YƸXWVUTꙄЯỌꟼOͶM⅃⋊ႱIHᎮꟻƎᗡƆᙠAʏƹxwvuƚꙅɿpqoᴎm|ʞꞁiʜǫᎸɘbɔdɒ"
+
+function cbulat(_in, _out){
+  var s="";
+  var n=_in.value.toLowerCase();
+  for(i=0; i<n.length; i++){
+    var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);
+    if (j<normal.length) {
+      s+=bulat.charAt(j);
+    } else {s+=c;}
+  }_out.value=s;
+}
+function cbulat2(_in, _out){var s="";var n=_in.value.toLowerCase();for(i=0; i<n.length; i++){var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);if (j<normal.length) {s+=bulat2.charAt(j);} else {s+=c;}}_out.value=s; }
+function cbulat3(_in, _out){var s="";var n=_in.value.toLowerCase();for(i=0; i<n.length; i++){var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);if (j<normal.length) {s+=bulat3.charAt(j);} else {s+=c;}}_out.value=s;}
+function csmallcap(_in, _out){var s="";var n=_in.value.toLowerCase();for(i=0; i<n.length; i++){var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);if (j<normal.length) {s+=smallcap.charAt(j);} else {s+=c;}}_out.value=s;}
+function ccool(_in, _out){var s="";var n=_in.value.toLowerCase();for(i=0; i<n.length; i++){var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);if (j<normal.length) {s+=cool.charAt(j);} else {s+=c;}}_out.value=s;}
+function csymbol(_in, _out){var s="";var n=_in.value.toLowerCase();for(i=0; i<n.length; i++){var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);if (j<normal.length) {s+=symbol.charAt(j);} else {s+=c;}}_out.value=s;}
+function crusia(_in, _out){var s="";var n=_in.value.toLowerCase();for(i=0; i<n.length; i++){var c=n.charAt(i);for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);if (j<normal.length) {s+=rusia1.charAt(j);} else {s+=c;}}_out.value=s;}
+
+function focusFirst() {if (els = oTD.getElementsByTagName("input")) {els[0].focus();}}
+function highlight(field) {field.focus();field.select();};
+
+function cbalik() {
+ var result = flipString(document.colongan.colong.value.toLowerCase());
+ document.colongan.colongin.value = result;
+}
+function flipString(aString) {
+  var last = aString.length - 1;
+  var result = new Array(aString.length)
+  for (var i = last; i >= 0; --i) { 
+    var c = aString.charAt(i)  
+    var r = flipTable[c]  
+    result[last - i] = r != undefined ? r : c 
+  } return result.join('')}
+var flipTable = {
+  'a' : '\u0250','b' : 'q','c' : '\u0254','d' : 'p','e' : '\u01DD','f' : '\u025F','g' : '\u0183','h' : '\u0265','i' : '\u0131','j' : '\u027E','k' : '\u029E','l' : 'l','m' : '\u026F','n' : 'u','o' : 'o','p' : 'd','q' : 'b','r' : '\u0279','s' : 's','t' : '\u0287','u' : 'n','v' : '\u028C','w' : '\u028D','y' : '\u028E','z' : 'z','1' : '\u21C2','2' : '\u1105','3' : '\u1110','4' : '\u3123','5' : '\u078E', '6' : '9','7' : '\u3125','8' : '8','9' : '6','0' : '0','.' : '\u02D9',',' : "\'","\'" : ',',"\"" : ',,',"?" : ',',"`" : ',',';' : '\u061B','!' : '\u00A1','\u00A1' : '!','?' : '\u00BF','\u00BF' : '?','[' : ']',']' : '[','(' : ')',')' : '(','{' : '}','}' : '{','<' : '>','>' : '<','_' : '\u203E','\r' : '\n' 
+}
+
+for (i in flipTable) {
+  flipTable[flipTable[i]] = i
+}
+
+var str; function atas() {
+  str = document.colongan.colong.value;
+  var chuncks = str.match(/.{1,1}/g);
+  var new_value1 = chuncks.join("̅");
+ document.colongan.colongin.value=new_value1 ;
+};
+var str; function bawah() {
+  str = document.colongan.colong.value;
+  var chuncks = str.match(/.{1,1}/g);
+  var new_value2 = chuncks.join("̲");
+ document.colongan.colongin.value=new_value2 ;
+};
+var str; function coret() {
+  str = document.colongan.colong.value;
+  var chuncks = str.match(/.{1,1}/g);
+  var new_value3 = chuncks.join("̶");
+ document.colongan.colongin.value=new_value3 ;
+};
+var str; function miring() {
+  str = document.colongan.colong.value;
+  var chuncks = str.match(/.{1,1}/g);
+  var new_value4 = chuncks.join("̸");
+ document.colongan.colongin.value=new_value4 ;
+};
+var str; function miring2() {
+  str = document.colongan.colong.value;
+  var chuncks = str.match(/.{1,1}/g);
+  var new_value5 = chuncks.join("̷");
+ document.colongan.colongin.value=new_value5 ;
+};
+var str; function derajat() {
+  str = document.colongan.colong.value;
+  var chuncks = str.match(/.{1,1}/g);
+  var new_value6 = chuncks.join("");
+ document.colongan.colongin.value=new_value6 ;
+};
+    var thisString; function tarty() { 
+thisString = document.colongan.colong.value;  
+ thisString = thisString.replace(/A/g,"░█▀▀█ \n▒█▄▄█ \n▒█░▒█ \n");
+ thisString = thisString.replace(/B/g,"▒█▀▀█ \n▒█▀▀▄ \n▒█▄▄█ \n");
+ thisString = thisString.replace(/C/g,"▒█▀▀█ \n▒█░░░ \n▒█▄▄█ \n");
+ thisString = thisString.replace(/D/g,"d");
+ thisString = thisString.replace(/E/g,"e");
+ thisString = thisString.replace(/F/g,"f");
+ thisString = thisString.replace(/G/g,"");
+ thisString = thisString.replace(/H/g,"");
+ thisString = thisString.replace(/I/g,"");
+ thisString = thisString.replace(/J/g,"");
+ thisString = thisString.replace(/K/g,"");
+ thisString = thisString.replace(/L/g,"");
+ thisString = thisString.replace(/M/g,"");
+ thisString = thisString.replace(/N/g,"");
+ thisString = thisString.replace(/O/g,"");
+ thisString = thisString.replace(/P/g,"");
+ thisString = thisString.replace(/Q/g,"");
+ thisString = thisString.replace(/R/g,"");
+ thisString = thisString.replace(/S/g,"");
+ thisString = thisString.replace(/T/g,"");
+ thisString = thisString.replace(/U/g,"");
+ thisString = thisString.replace(/V/g,"");
+ thisString = thisString.replace(/W/g,"");
+ thisString = thisString.replace(/X/g,"");
+ thisString = thisString.replace(/Y/g,"");
+ thisString = thisString.replace(/Z/g,"");
+ thisString = thisString.replace(/a/g,"");
+ thisString = thisString.replace(/b/g,"");
+ thisString = thisString.replace(/c/g,"");
+ thisString = thisString.replace(/d/g,"");
+ thisString = thisString.replace(/e/g,"");
+ thisString = thisString.replace(/f/g,"");
+ thisString = thisString.replace(/g/g,"");
+ thisString = thisString.replace(/h/g,"");
+ thisString = thisString.replace(/i/g,"");
+ thisString = thisString.replace(/j/g,"");
+ thisString = thisString.replace(/k/g,"");
+ thisString = thisString.replace(/l/g,"");
+ thisString = thisString.replace(/m/g,"");
+ thisString = thisString.replace(/n/g,"");
+ thisString = thisString.replace(/o/g,"");
+ thisString = thisString.replace(/p/g,"");
+ thisString = thisString.replace(/q/g,"");
+ thisString = thisString.replace(/r/g,"");
+ thisString = thisString.replace(/s/g,"");
+ thisString = thisString.replace(/t/g,"");
+ thisString = thisString.replace(/u/g,"");
+ thisString = thisString.replace(/v/g,"");
+ thisString = thisString.replace(/w/g,"");
+ thisString = thisString.replace(/x/g,"");
+ thisString = thisString.replace(/y/g,"");
+ thisString = thisString.replace(/z/g,"");
+ thisString = thisString.replace(/1/g,"");
+ thisString = thisString.replace(/2/g,"");
+ thisString = thisString.replace(/3/g,"");
+ thisString = thisString.replace(/4/g,"");
+ thisString = thisString.replace(/5/g,"");
+ thisString = thisString.replace(/6/g,"");
+ thisString = thisString.replace(/7/g,"");
+ thisString = thisString.replace(/8/g,"");
+ thisString = thisString.replace(/9/g,"");
+ thisString = thisString.replace(/0/g,"");
+ document.colongan.colongin.value=thisString ;};
